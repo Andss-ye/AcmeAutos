32ebe7e4d47b517947bacd806dca45f9fdc12cb8
Andrew <andrex120tv@gmail.com>
2024-11-01 14:48:51 -0500

    feat: :rocket: Se agrego el menu principal del programa

---
+++ main.py
@@ -0,0 +1,27 @@
+def menu():
+    print("\n=========== BIENVENIDO ===========")
+    print("1. Registrar vehiculo en exhibicion")
+    print("2. Registrar venta de vehiculo")
+    print("3. Listar vehiculos")
+    print("3. Generar reporte de ventas por mes")
+    print("0. Exit")
+    print("=================================")
+    opc = int(input("Ingrese una opcion: "))
+    return opc
+
+opc = 10
+
+while opc != 0:
+    opc = menu()
+    if opc == 1:
+        print("Registrar vehiculo en exhibicion")
+    elif opc == 2:
+        print("Registrar venta de vehiculo")
+    elif opc == 3:
+        print("Listar vehiculos")
+    elif opc == 4:
+        print("Generar reporte de ventas por mes")
+    elif opc == 0:
+        print("Exit")
+    else:
+        print("Opcion incorrecta")
