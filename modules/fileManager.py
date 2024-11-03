459633406a5d4d0bd8c457fbbb507bd520ac39bb
Jeronimo <rugejero06@gmail.com>
2024-11-02 21:01:33 -0500

    feat: :rocket: se agrego la funcion para generar el reporte en csv en el archivo file manager

--- modules/fileManager.py
+++ modules/fileManager.py
@@ -1,4 +1,5 @@
 import json
+import csv 
 
 def loadVehicles():
     try:
@@ -14,3 +15,9 @@ def loadVehicles():
 def saveVehicles(vehiculos):
     with open('bd/vehiculos.json', 'w') as file:
         json.dump(vehiculos, file, indent=4)
+
+def reportExibition(report):
+    with open('bd/report_Exibition.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['codigo', 'marca', 'modelo', 'año de lanzamiento'])
+        writer.writerows(report)
